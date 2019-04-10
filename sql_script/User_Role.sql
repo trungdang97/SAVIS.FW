74df9cb9d5d176533e90cdbba8d03c3b22a5142d
trungdang97 <35669562+trungdang97@users.noreply.github.com>
2019-04-11 01:35:36 +0700

    update sql scheme

--- sql_script/User_Role.sql
+++
@@ -1,7 +0,0 @@
-use BloodBankSystem
-go
-create table bbs_Role(RoleId int identity(1,1) primary key, RoleName nvarchar(50) not null, PermissionId int) 
-go
-create table bbs_User(UserId uniqueidentifier default newid() primary key, UserName varchar(128) not null, HashString varchar(64) not null, 
-	RoleId int not null constraint FK_User_Role foreign key(RoleId) references bbs_Role(RoleId))
-go
